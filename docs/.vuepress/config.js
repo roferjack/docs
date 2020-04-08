d80d89546f68c815f4d0d1020f22a98767b5875f
rofer_ky <kaiyoulijack@gmail.com>
2020-04-08 22:24:30 +0800

    pwa设置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,15 @@ module.exports = {
     head: [
       ['link', { rel: 'icon', href: '/favicon.ico' }],
       ['meta', { name: 'author', content:'消失的烟雨' }],  
-      ['meta', { name: 'keywords', content:'消失的烟雨,伪前端的咸鱼' }]      
+      ['meta', { name: 'keywords', content:'消失的烟雨,伪前端的咸鱼' }],
+      ['link', { rel: 'manifest', href: '/manifest.json' }],
+      ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+      ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+      ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+      ['link', { rel: 'apple-touch-icon', href: 'images/icons/icon-152x152.png' }],
+      // ['link', { rel: 'mask-icon', href: 'images/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+      ['meta', { name: 'msapplication-TileImage', content: 'images/icons/icon-144x144.png' }],
+      ['meta', { name: 'msapplication-TileColor', content: '#000000' }]            
     ],
  
     themeConfig: {
@@ -117,7 +125,7 @@ module.exports = {
     
     base: '/docs/',
     plugins: [
-      'vuepress-plugin-nprogress',
+      ['vuepress-plugin-nprogress'],
       [
         'vuepress-plugin-zooming',
         {
@@ -130,7 +138,16 @@ module.exports = {
         },
 
       ],
-      '@vuepress/back-to-top'
+      ['@vuepress/back-to-top'],
+      ['@vuepress/pwa',
+        {
+          serviceWorker: true,
+          updatePopup: {
+            message: "发现新内容可用",
+            buttonText: "刷新"
+          }
+        }
+      ]
     ]
 
 
